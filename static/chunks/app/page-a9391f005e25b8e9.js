ae9cbdb545554baab2adce940970ee8b8f43ccbd
gr-gasic <136888760+gr-gasic@users.noreply.github.com>
2023-11-05 13:25:49 +0900

    Add files via upload

---
+++ static/chunks/app/page-a9391f005e25b8e9.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{7781:function(e,r,t){Promise.resolve().then(t.bind(t,3567))},3567:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return Home}});var n=t(7437),o=t(4033);function Home(){let e=(0,o.useRouter)();return setTimeout(()=>e.push("https://discord.gg/cNQ26ANK"),1e3),(0,n.jsx)("h1",{children:"잠시만요 이동중.."})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),o=Symbol.for("react.element"),u=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,t){var n,c={},i=null,_=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(_=r.ref),r)u.call(r,n)&&!f.hasOwnProperty(n)&&(c[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===c[n]&&(c[n]=r[n]);return{$$typeof:o,type:e,key:i,ref:_,props:c,_owner:s.current}}r.jsx=q},7437:function(e,r,t){"use strict";e.exports=t(622)},4033:function(e,r,t){e.exports=t(94)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=7781)}),_N_E=e.O()}]);
